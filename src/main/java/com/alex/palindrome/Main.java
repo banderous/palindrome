abd17ebaf770fb57a5d79f34dc71a5a945c409a7
Alex McAusland <alex.mcausland@gmail.com>
2017-12-05 20:24:03 +0000

    adding main method

--- src/main/java/com/alex/palindrome/Main.java
+++ src/main/java/com/alex/palindrome/Main.java
@@ -1,10 +1,20 @@
 package com.alex.palindrome;
 
+import java.util.List;
+
 /**
  * Created by alex on 04/12/2017.
  */
 public class Main {
     public static void main(String[] args) {
-       System.out.println("goat");
+        if (args.length != 1) {
+            System.out.println("Usage: input");
+            return;
+        }
+        List<List<String>> summary = Palindrome.longest3Palindromes(args[0]);
+        for (List<String> strings : summary) {
+            String line = String.format("Text: %s, Index: %s, Length: %s", strings.toArray());
+            System.out.println(line);
+        }
     }
 }
